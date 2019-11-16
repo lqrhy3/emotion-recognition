0bc8d756c24f05b7a038e61070e520136b356e25
JJBT <chernvld@gmail.com>
2019-11-16 22:45:21 +0300

    class dataset for detection

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,17 +1,32 @@
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
+import os
+import cv2
+import numpy as np
+import json
 
 
-class MyDataset(Dataset):
-    def __init__(self):
-        pass
+class DetectDataset(Dataset):
+    """Face detection dataset"""
+    dir_ = os.path.dirname(__file__)
+
+    def __init__(self, path='../data/detection/'):
+        self.datadir = os.path.join(self.dir_, path, 'images')
+        self.ground_tr_dir = os.path.join(self.dir_, path, 'dict_metadata.txt')
+        self.ground_tr = json.load(open(self.ground_tr_dir, 'r'))
+        self.images = np.array(os.listdir(self.datadir))
 
     def __getitem__(self, item):
-        pass
+        if torch.is_tensor(item):
+            item = item.tolist()
+
+        img_name = os.path.join(self.datadir, self.images[item])
+        image = cv2.imread(img_name)
+
+        face_rect = self.ground_tr[self.images[item][:16]]
+        sample = (image, list(map(int, face_rect)))
+
+        return sample
 
     def __len__(self):
-        pass
-
-    def collate_fn(self, batch):
-        pass
+        return len(self.images)
