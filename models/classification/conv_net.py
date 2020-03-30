8f8b6e8187586fce46c22d0cf2664e830c1e55fb
Stas Mikhaylevskiy <m1xst99@yandex.ru>
2020-03-30 18:54:19 +0300

    ConvNet.fuse_model() added

--- models/classification/conv_net.py
+++ models/classification/conv_net.py
@@ -2,27 +2,9 @@ import torch
 from torch import nn
 
 
-class ConvNet(nn.Module):
-    """ResNet-based classification model.
-        """
-    def __init__(self, emotion_map):
-        """:param: emotion_map: emotion type list"""
-        super(ConvNet, self).__init__()
-
-        self.emotion_map = emotion_map
-
-        self.layer1 = self._make_conv_block(in_channels=3, out_channels=16)
-        self.layer2 = self._make_conv_block(in_channels=16)
-        self.layer3 = self._make_conv_block(in_channels=32)
-        self.layer4 = self._make_conv_block(in_channels=64)
-        self.layer5 = self._make_conv_block(in_channels=128)
-        self.layer6 = self._make_conv_block(in_channels=256)
-
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.fc = nn.Linear(in_features=512, out_features=len(emotion_map))
-
-    @staticmethod
-    def _make_conv_block(in_channels, out_channels=None):
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels=None):
+        super(ConvBlock, self).__init__()
 
         if out_channels is None:
             out_channels = in_channels * 2
@@ -35,15 +17,42 @@ class ConvNet(nn.Module):
             stride=1,
             padding=1,
             bias=False
-            )
+        )
         )
         layers.append(nn.BatchNorm2d(num_features=out_channels))
-        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         layers.append(nn.ReLU())
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
-        return nn.Sequential(*layers)
+        self.layer = nn.Sequential(*layers)
 
     def forward(self, x):
+        return self.layer(x)
+
+
+class ConvNet(nn.Module):
+    """ResNet-based classification model.
+        """
+    def __init__(self, emotion_map):
+        """:param: emotion_map: emotions list"""
+        super(ConvNet, self).__init__()
+
+        self.emotion_map = emotion_map
+
+        self.layer1 = ConvBlock(in_channels=3, out_channels=16)
+        self.layer2 = ConvBlock(in_channels=16)
+        self.layer3 = ConvBlock(in_channels=32)
+        self.layer4 = ConvBlock(in_channels=64)
+        self.layer5 = ConvBlock(in_channels=128)
+        self.layer6 = ConvBlock(in_channels=256)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Linear(in_features=512, out_features=len(emotion_map))
+
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
+
+    def forward(self, x):
+        x = self.quant(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
@@ -54,7 +63,13 @@ class ConvNet(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
+        x = self.dequant(x)
         return x
+
+    def fuse_model(self):
+        for m in self.children():
+            if isinstance(m, ConvBlock):
+                torch.quantization.fuse_modules(m.layer, [['0', '1', '2']], inplace=True)
 
 
 class PretrConvNet(nn.Module):
@@ -74,3 +89,12 @@ class PretrConvNet(nn.Module):
     def forward(self, x):
         x = self.backbone(x)
         return x
+
+
+if __name__ == '__main__':
+    model = ConvNet([''])
+    model.fuse_model()
+    model.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
+    torch.quantization.prepare(model, inplace=True)
+    torch.quantization.convert(model, inplace=True)
+    print(model)
